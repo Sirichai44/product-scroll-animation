3f894e0516413297243a3035899308bf334961c7
Sirichai99 <125535519+Sirichai99@users.noreply.github.com>
2023-06-19 11:56:34 +0700

    Add files via upload

---
+++ script.js
@@ -0,0 +1,18 @@
+const categories = document.querySelectorAll('.category');
+
+window.addEventListener('scroll', showCategory);
+
+function showCategory(){
+    const calculateHeight = window.innerHeight - 200;
+
+    categories.forEach(category =>{
+        const topPosition = category.getBoundingClientRect().top;
+        
+        if(topPosition < calculateHeight){
+            category.classList.add('active');
+        }else{
+            category.classList.remove('active');
+        }
+    
+    });
+}
